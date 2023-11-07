cc1f33acecd48c9af4eea0872e6e8e2219edece1
Francisco Cano <franffcano@gmail.com>
2023-11-07 17:05:32 -0300

    instale Toastify mediante npm, y modifique el archivo js para que pueda salir una notificacion customizada con la libreria, aunque todavia no logro hacerla funcionar

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+// Import para que funcione Toastify
+import Toastify from 'toastify-js';
+
 // Función para cargar el archivo JSON
 fetch('api.json')
 .then(response => response.json())
@@ -28,42 +31,34 @@ fetch('api.json')
     });
 
 
-    
-    // Función para comparar dos placas de video
-    function compararPlacas() {
-        if (placaSeleccionada1 && placaSeleccionada2) {
-            const rendimiento1 = placaSeleccionada1.rendimiento;
-            const rendimiento2 = placaSeleccionada2.rendimiento;
-            const nucleos1 = placaSeleccionada1.nucleos;
-            const ram1 = placaSeleccionada1.ram;
-            const nucleos2 = placaSeleccionada2.nucleos;
-            const ram2 = placaSeleccionada2.ram;
-            const precio1 = placaSeleccionada1.precio;
-            const precio2 = placaSeleccionada2.precio;
-            const lanzamiento1 = placaSeleccionada1.lanzamiento;
-            const lanzamiento2 = placaSeleccionada2.lanzamiento;
+    // Función para comparar dos placas de video y mostrar la notificación con Toastify
+function compararPlacas() {
+    if (placaSeleccionada1 && placaSeleccionada2) {
+        const rendimiento1 = placaSeleccionada1.rendimiento;
+        const rendimiento2 = placaSeleccionada2.rendimiento;
+        const nucleos1 = placaSeleccionada1.nucleos;
+        const ram1 = placaSeleccionada1.ram;
+        const nucleos2 = placaSeleccionada2.nucleos;
+        const ram2 = placaSeleccionada2.ram;
+        const precio1 = placaSeleccionada1.precio;
+        const precio2 = placaSeleccionada2.precio;
+        const lanzamiento1 = placaSeleccionada1.lanzamiento;
+        const lanzamiento2 = placaSeleccionada2.lanzamiento;
 
-            const comparacionData = {
-                placa1: placaSeleccionada1,
-                placa2: placaSeleccionada2,
-                resultado: `Comparación de rendimiento:
-                    ${placaSeleccionada1.placaDeVideo} (${rendimiento1} puntos, ${nucleos1} núcleos, ${ram1} RAM, ${precio1} USD, ${lanzamiento1}) vs. 
-                    ${placaSeleccionada2.placaDeVideo} (${rendimiento2} puntos, ${nucleos2} núcleos, ${ram2} RAM, ${precio2} USD, ${lanzamiento2})`
-            };
+        const comparacionMensaje = `Comparación de rendimiento:
+            ${placaSeleccionada1.placaDeVideo} (${rendimiento1} puntos, ${nucleos1} núcleos, ${ram1} RAM, ${precio1} USD, ${lanzamiento1}) vs. 
+            ${placaSeleccionada2.placaDeVideo} (${rendimiento2} puntos, ${nucleos2} núcleos, ${ram2} RAM, ${precio2} USD, ${lanzamiento2})`;
 
-            localStorage.setItem('comparacion', JSON.stringify(comparacionData));
+        // Mostrar la notificación de Toastify
+        Toastify({
+            text: comparacionMensaje,
+            duration: 5000, // Duración de la notificación en milisegundos (5 segundos en este caso)
+        }).showToast();
 
-            const comparacionDiv = document.createElement("div");
-            comparacionDiv.classList.add("comparacion");
-            const resultado = document.createElement("p");
-            resultado.textContent = comparacionData.resultado;
-            comparacionDiv.appendChild(resultado);
-            document.body.appendChild(comparacionDiv);
-
-            placaSeleccionada1 = null;
-            placaSeleccionada2 = null;
-        }
+        placaSeleccionada1 = null;
+        placaSeleccionada2 = null;
     }
+}
 
     function mostrarComparacionGuardada() {
         const comparacionData = localStorage.getItem('comparacion');
@@ -84,6 +79,7 @@ fetch('api.json')
 .catch(error => {
     console.error('Error al cargar el archivo JSON', error);
 });
+
 
 
 
